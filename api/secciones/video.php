04795f8da9b3dc4dcc045688ae80020a885db5a4
Rodrigo Garcia <rodri0259@hotmail.com>
2024-11-06 11:46:03 -0500

    Update video.php

--- api/secciones/video.php
+++ api/secciones/video.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $video = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Y pudo ser peor: Real Madrid perdió 3-1 ante Milan en el Bernabéu por la Champions League",
+"autor" => "Champions League",
+"resumen" => "El Real Madrid está en crisis. Todos los malos síntomas de una temporada sin fútbol se confirmaron con su segunda derrota consecutiva en el Santiago Bernabéu. Goleado por el Barcelona en el clásico liguero y con una debilidad alarmante cuando se esperaba la reacción en Europa",
 ];
 ?>
