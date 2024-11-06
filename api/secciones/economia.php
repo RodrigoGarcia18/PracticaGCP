f1a43971daf1a749bc87b3073e095aaa94eca3f0
Rodrigo Garcia <rodri0259@hotmail.com>
2024-11-06 11:31:17 -0500

    Update economia.php

--- api/secciones/economia.php
+++ api/secciones/economia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $economia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Siguen atrapados en un incómodo punto muerto",
+"autor" => "Martín Tumay Soto",
+"resumen" => "Aunque Perú mantiene su calificación en BBB por su estabilidad macrofinanciera, la agencia advirtió que las tensiones políticas y la baja inversión privada podrían limitar el crecimiento económico en los próximos años",
 ];
 ?>
