e6a4760d922f8a6cac2dec12bb595685d1ce805f
Rodrigo Garcia <rodri0259@hotmail.com>
2024-11-06 11:42:01 -0500

    Update opinion.php

--- api/secciones/opinion.php
+++ api/secciones/opinion.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $opinion = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Bono S/380 y S/500 en noviembre 2024: ",
+"autor" => "Diego Pecho",
+"resumen" => "El Estado peruano ha anunciado la entrega de subsidios a lo largo del mes de noviembre, con el objetivo de brindar apoyo a aquellos ciudadanos que enfrentan dificultades económicas",
 ];
 ?>
