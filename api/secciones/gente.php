1c2e8c114929672f66b815df01eb61607b8bb1a8
Rodrigo Garcia <rodri0259@hotmail.com>
2024-11-06 11:34:59 -0500

    Update gente.php

--- api/secciones/gente.php
+++ api/secciones/gente.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $gente = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "El entrenamiento militar de la princesa Victoria de Suecia, en imágenes",
+"autor" => "El País",
+"resumen" => "Disparando fusiles automáticos, utilizando artillería de mortero, atravesando un río, desplazándose por cuerdas suspendidas, de rodillas y completando circuitos complicados. ",
 ];
 ?>
