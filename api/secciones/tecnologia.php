458952dd024a74b48f2f258b48f14a04f94ea262
Rodrigo Garcia <rodri0259@hotmail.com>
2024-11-06 11:43:21 -0500

    Update tecnologia.php

--- api/secciones/tecnologia.php
+++ api/secciones/tecnologia.php
@@ -4,7 +4,7 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $tecnologia = [
-"titulo" => "XXX",
+"titulo" => "Ivo Saona, director general de Aporta (Breca) explica la importancia de implementar la cultura consciente dentro de las organizaciones para promover valores que fomenten el bienestar colectivo.",
 "autor" => "XXX",
 "resumen" => "XXX",
 ];
