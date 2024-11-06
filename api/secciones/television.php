3d7ec6866872ebccf1e260a5369ec1d5a2a676ad
Rodrigo Garcia <rodri0259@hotmail.com>
2024-11-06 11:44:44 -0500

    Update television.php

--- api/secciones/television.php
+++ api/secciones/television.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $television = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Aeropuerto Jorge Chávez: pasajeros de JetSmart protestan por cancelación de vuelos",
+"autor" => "TVPeru",
+"resumen" => "Decenas de pasajeros de la aerolínea JetSmart expresaron su malestar por la reprogramación y cancelación de sus vuelos al interior del país, lo que les hizo perder importantes compromisos laborales y familiares.",
 ];
 ?>
