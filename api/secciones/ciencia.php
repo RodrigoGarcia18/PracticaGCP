93e6d7cae45e30e4cb175bf6908b77af30a83384
Rodrigo Garcia <rodri0259@hotmail.com>
2024-11-06 11:29:07 -0500

    Update ciencia.php

--- api/secciones/ciencia.php
+++ api/secciones/ciencia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $ciencia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Hallado el denominador común para todos los cánceres",
+"autor" => "Javier Flores",
+"resumen" => "Científicos del Sinai Health han encontrado que todos los cánceres se dividen en solo dos categorías. Los hallazgos podrían proporcionar una nueva estrategia para tratar las formas más agresivas de la enfermedad.",
 ];
 ?>
