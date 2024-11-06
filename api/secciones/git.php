f340d43d3e8e2de879a8be56f473773f051fc66a
Rodrigo Garcia <rodri0259@hotmail.com>
2024-11-06 11:36:34 -0500

    Update git.php

--- api/secciones/git.php
+++ api/secciones/git.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $git = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "15,000 Credenciales Robadas y 10,000 Repositorios Privados en Riesgo",
+"autor" => "Pedro Fernandez ",
+"resumen" => "Un grupo de investigadores en ciberseguridad ha descubierto una campaña masiva que se aprovecha de configuraciones inseguras de Git, esté es un sistema de control de versiones que permite a los desarrolladores gestionar y coordinar cambios en el código fuente. ",
 ];
 ?>
