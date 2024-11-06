343bb604a856b30559295ed940becdd0ce75b3e5
Rodrigo Garcia <rodri0259@hotmail.com>
2024-11-06 11:30:20 -0500

    Update cultura.php

--- api/secciones/cultura.php
+++ api/secciones/cultura.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $cultura = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Aprueban la primera Política Nacional de Cultura del Perú",
+"autor" => "Estado Peruano",
+"resumen" => "En el décimo aniversario del Ministerio de Cultura, se publicó el instrumento que favorecerá el ejercicio pleno de los derechos culturales de los peruanos y peruanas.",
 ];
 ?>
