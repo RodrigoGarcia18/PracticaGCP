f65794b88c7b589021278160cbbfe20114843e6b
Rodrigo Garcia <rodri0259@hotmail.com>
2024-11-06 11:38:44 -0500

    Update openshift.php

--- api/secciones/openshift.php
+++ api/secciones/openshift.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $openshift = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Openshift AI y RHEL AI: sinergia entre inteligencia artificial y código abierto",
+"autor" => "Victor Chavez",
+"resumen" => "Tanto Openshift AI como RHEL AI son plataformas basadas en el modelo de código abierto, lo que abre un mundo de posibilidades en los negocios.",
 ];
 ?>
