5620d067dec153c8a44cc5fe4c520e716b272604
Rodrigo Garcia <rodri0259@hotmail.com>
2024-11-06 11:32:02 -0500

    Update empleo.php

--- api/secciones/empleo.php
+++ api/secciones/empleo.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $empleo = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Así postulas a la convocatoria laboral para personas sin experiencia del 5 de noviembre:",
+"autor" => "Jordan Arce",
+"resumen" => "El Ministerio de Trabajo y Promoción del Empleo (MTPE) anunció una próxima convocatoria laboral que se desarrollará este martes 5 de noviembre. El evento se llevará a cabo con la finalidad de atraer talentos y abrir puertas a diversos perfiles.",
 ];
 ?>
