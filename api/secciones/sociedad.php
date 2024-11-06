4fb05f0c86d72d664bd6139e9797250f955d16a1
Rodrigo Garcia <rodri0259@hotmail.com>
2024-11-06 11:42:40 -0500

    Update sociedad.php

--- api/secciones/sociedad.php
+++ api/secciones/sociedad.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $sociedad = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Liderazgo consciente: transformando culturas y vidas",
+"autor" => "Ivo Saona",
+"resumen" => "Ivo Saona, director general de Aporta (Breca) explica la importancia de implementar la cultura consciente dentro de las organizaciones para promover valores que fomenten el bienestar colectivo.",
 ];
 ?>
