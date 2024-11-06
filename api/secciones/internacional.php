4232a0cec04ba8f8bea3977ff9f2a722453c125d
Rodrigo Garcia <rodri0259@hotmail.com>
2024-11-06 11:37:22 -0500

    Update internacional.php

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Cómo Donald Trump volvió del abismo político",
+"autor" => "Anthony Zurcher",
+"resumen" => "En la Conferencia de Acción Política Conservadora (CPAC, por sus siglas en inglés), un encuentro de derechas que se celebra en Estados Unidos, el expresidente demostró que todavía contaba con la lealtad de la base republicana.",
 ];
 ?>
