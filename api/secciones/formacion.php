b81bf51e8c4e9e2f5748176cceea9441f079332d
Rodrigo Garcia <rodri0259@hotmail.com>
2024-11-06 11:33:42 -0500

    Update formacion.php

--- api/secciones/formacion.php
+++ api/secciones/formacion.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $formacion = [
-"titulo" => "XXX",
-"autor" => "sistemas@uns",
-"resumen" => "XXX",
+"titulo" => "Fórum Intercultural Perú – China: La labor de los Medios de Comunicación en la Educación",
+"autor" => "Ministerio del Peru",
+"resumen" => "Este miércoles 6 de noviembre, el Congreso de la República será escenario del Fórum Intercultural Perú – China: “Labor de los Medios de Comunicación en la Educación”, un evento organizado por el Bloque Magisterial de Concertación Nacional, que busca fortalecer los intercambios de cooperación entre Perú y China en el ámbito educativo. La actividad se desarrollará a partir de las 3:00 p. m. en el auditorio Alberto Andrade Carmona, en el edificio Juan Santos Atahualpa del Congreso.",
 ];
 ?>
