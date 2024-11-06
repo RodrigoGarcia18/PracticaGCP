b4606abe20ece5785837beaaa646cfa1671da0d7
Rodrigo Garcia <rodri0259@hotmail.com>
2024-11-06 11:40:48 -0500

    Update openstack.php

--- api/secciones/openstack.php
+++ api/secciones/openstack.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $openstack = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Red Hat OpenStack Platform 13 simplifica las actualizaciones con funciones de avance rápido",
+"autor" => "Red Hat",
+"resumen" => "Red Hat ha anunciado Red Hat OpenStack Platform 13, la última versión de la solución de infraestructura como servicio (IaaS) en la nube masivamente escalable y ágil de Red Hat",
 ];
 ?>
