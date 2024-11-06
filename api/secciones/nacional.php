add9fa188c994719cfd812e5fa70d55fbf88484b
Rodrigo Garcia <rodri0259@hotmail.com>
2024-11-06 11:38:06 -0500

    Update nacional.php

--- api/secciones/nacional.php
+++ api/secciones/nacional.php
@@ -5,7 +5,7 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 
 $nacional = [
 "titulo" => "Temblor en Perú, hoy 6 de noviembre",
-"autor" => "XXX",
-"resumen" => "XXX",
+"autor" => "Redacción RPP",
+"resumen" => "Es importante construir una cultura de prevención que ayude a mitigar los daños que podría ocasionar un evento de grandes magnitudes. Es responsabilidad nuestra participar y tomar con seriedad los simulacros de sismos que organiza el Instituto Nacional de Defensa Civil (Indeci); así como los que se hacen en casa, en el trabajo o en los centros de estudios.",
 ];
 ?>
